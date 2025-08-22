095ea33247ce25206293ebff2bf3203008c79299
shohjahon <xayrullayevshohjahon871@gmail.com>
2025-08-22 18:09:48 +0500

    Add files via upload

---
+++ socript.js
@@ -0,0 +1,11 @@
+let arr = []
+
+let c1 = prompt("1-moshinani kiriting")
+let c2 = prompt("2-moshinani kiriting")
+let c3 = prompt("3-moshinani kiriting")
+
+arr.push(c1);
+arr.push(c2);
+arr.push(c3);
+
+console.log(arr);
